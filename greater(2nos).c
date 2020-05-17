f6b30120becb4ccb9c87a6ced3cd776b2cf88fff
pragatii-14 <65433451+pragatii-14@users.noreply.github.com>
2020-05-17 22:40:03 +0530

    Q.1 Maximum between two
    C code 
    WAP tp find maximum between two

---
+++ greater(2nos).c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+void main()
+{
+	int a,b;
+	printf("enter 1st no.:");;
+	scanf("%d",&a);
+	printf("enter 2nd no.:");
+	scanf("%d",&b);
+	if(a==b)
+	printf("both nos. are equal");
+	if(a>b)
+	printf("a is geater than b");
+	else
+	printf("b is greater than a");
+}
